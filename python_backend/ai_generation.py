0f5c6343d38a613edaf14beb1fb5fca93ac395b4
Andy Tong <andytong369@gmail.com>
2025-02-05 21:06:47 -0500

    Update ai_generation.py

--- python_backend/ai_generation.py
+++ python_backend/ai_generation.py
@@ -1,17 +1,24 @@
 from openai import OpenAI
 from api_key_database import *
 
+instructions = f"You will be provided with a list of item names with IDs. Ignored the IDs of each item. " \
+               f"Consider each item carefully and how they may relate to completing the user-given prompt. " \
+               f"Your primary task is to help complete the user's prompt. However, if the user-given prompt " \
+               f"is unclear or lacks any details you need, you must tell them to restate their prompt "\
+               f"the clarifications or details necessary and click the \"Generate\" button again. "
+
+messages_list = [{"role":"system", "content": instructions},
+                 {"role":"user", "content": "I know there's something in the wake of your smile"},
+                 {"role":"assistant", "content": "I get a notion from the look in your eyes, yeah"},
+                 {"role":"user", "content": "You've built a love but that love falls apart"},
+                 {"role":"assistant", "content": "Your little piece of Heaven turns too dark"},
+                 {"role":"user", "content": "Listen to your"}]
+
 def general_request(listOfItems, userInput):
     key = get_api_key()
     client = OpenAI(
         api_key=key
     )
-    instructions = f"You will be provided with a list of items names with IDs. Ignored the IDs of each item. " \
-                   f"Consider each item carefully and how they may relate to completing the user-given prompt. " \
-                   f"The order of items in the list may or may not matter based on the user prompt. If the " \
-                   f"user-given prompt is unclear or lacks any details, tell them to restate their prompt but with" \
-                   f"the clarifications or details necessary. You must tell them to restate and click" \
-                   f"the \"Generate\" button again if this is the case."
 
     inputPrompt = f"Here is the aforementioned list of items in the form of an array: {listOfItems}. " \
                   f"Only use this list and no other mentioned lists. Each line must be at most" \
@@ -24,8 +31,11 @@ def general_request(listOfItems, userInput):
         {"role": "system", "content": instructions},
         {"role": "user", "content": inputPrompt}
       ],
-        temperature=0.55
+        temperature=0.6
     )
+    result = completion.choices[0].message.content
+    new_message = {"role": "assistant", "content": result}
+    messages_list.append(new_message)
     return completion.choices[0].message.content
 
 # TO DO: Implement this as a preset
